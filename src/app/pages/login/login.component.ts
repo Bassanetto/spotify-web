eb1212561f6ca955c374f5c3127c41c154c0260e
“Bassanetto” <gustavo.almeidabassanetto@gmail.com>
2023-11-10 15:13:55 -0300

    Spotify :: Documentação da tela de Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,8 @@
+/**
+ * Representa o componente de login da aplicação.
+ *
+ * Este componente permite que os usuários acessem a aplicação por meio do serviço do Spotify.
+ */
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from "../../services/spotify.service";
 import { Router } from "@angular/router";
@@ -9,23 +14,40 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
+  /**
+   * Construtor da classe LoginComponent.
+   *
+   * @param spotifyService - O serviço do Spotify utilizado para autenticação.
+   * @param router - O serviço de roteamento do Angular.
+   */
   constructor(
     private spotifyService: SpotifyService,
-    private router: Router,) { }
+    private router: Router,
+  ) { }
 
+  /**
+   * Método de inicialização do Angular executado quando o componente é criado.
+   * Verifica se há um token de autenticação na URL e, se houver, define-o como token de acesso e redireciona para a página do jogador.
+   */
   ngOnInit(): void {
     this.verificarTokenUrlCallback();
   }
 
+  /**
+   * Verifica a URL para um token de autenticação e, se encontrado, define-o como token de acesso e redireciona para a página do jogador.
+   */
   verificarTokenUrlCallback() {
     const token = this.spotifyService.obterTokenUrlCallback();
-    if(!!token){
+    if (!!token) {
       this.spotifyService.definirAccessToken(token);
-      this.router.navigate(['/player/home']);
+      this.router.navigate(['/player']);
     }
   }
 
+  /**
+   * Abre a página de login do Spotify redirecionando o usuário para a URL de login.
+   */
   abrirPaginaLogin() {
     window.location.href = this.spotifyService.obterUrlLogin();
-  }
+  }
 }
